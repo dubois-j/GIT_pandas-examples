637ea4bc7d9d1799c15de63ab54d731f34faef94
Wissal <wissbaa@gmail.com>
2025-08-07 09:32:31 +0200

    author

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+sortR= cloneDF(mergeRatings)
+sortR= sortR.groupby(['movie_id', 'title'])['rating'].agg(
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+print('My info sorted: \n%s' % sortR[:15])
