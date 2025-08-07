07d08f65fc7738f7771582a438319accb3077fa3
Mehdi Chouchane <mehdi.chouchan@gmail.com>
2025-08-07 09:23:56 +0200

    Rename SortedRatings to CommunicationNonViolente

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+CommunicationNonViolente = cloneDF(mergeRatings)
+CommunicationNonViolente = CommunicationNonViolente.groupby(['movie_id', 'title'])['rating'].agg(
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+print('My info sorted: \n%s' % CommunicationNonViolente[:15])
